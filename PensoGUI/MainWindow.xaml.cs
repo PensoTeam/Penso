f979cde1558a94a2f2f9d159b1fae1c5a804b4f9
LeeJoonHee <neo81389@gmail.com>
2022-01-09 15:56:42 +0900

    feat(CursorEvent): comment added

--- PensoGUI/MainWindow.xaml.cs
+++ PensoGUI/MainWindow.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -17,70 +13,51 @@ namespace PensoGUI
 
     public partial class MainWindow : Window
     {
-
-        //[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        //public static extern void MouseEvent(int dwFlag, int dx, int dy, int cButtons, int dwExtraInfo);
-        
-        [DllImport("User32")]
+        [DllImport("User32")] // * need 'User32.dll' for mouse position setting
         public static extern int SetCursorPos(int x, int y);
-
-        private const int MouseEventMove = 0x0001;
-        private const int MouseEventLeftDown = 0x0001;
-        private const int MouseEventLeftUp = 0x0004;
-        private const int MouseEventRightDown= 0x0008;
-
         private readonly ManualResetEvent stoppingEvent = new ManualResetEvent(false);
         private TimeSpan interval_;
 
         public MainWindow()
         {
+            // init
             InitializeComponent();
-            
             interval_ = TimeSpan.FromMilliseconds(1000);
             stoppingEvent.Reset();
             
+            // set to 200, 200
             MouseSetPosCustom(200,200);
             
+            // mouse position Label
             Label label = new Label();
             label.Width = 100; 
             label.Height = 30;
             this.Content = label;
+            
+            // get mouse position & print : loop
             this.Loaded += delegate { 
                 System.Timers.Timer timer = new System.Timers.Timer(); 
                 timer.Elapsed += delegate { 
                     this.Dispatcher.Invoke(new Action(delegate { 
                         Mouse.Capture(this); 
                         Point pointToWindow = Mouse.GetPosition(this); 
-                        Point pointToScreen = PointToScreen(pointToWindow);
+                        Point pointToScreen = PointToScreen(pointToWindow); 
                         label.Content = pointToScreen.ToString(); 
                         Mouse.Capture(null); 
                     })); 
                 };
+                
                 timer.Interval = 1;
                 timer.Start(); };
         }
 
-        public void MouseClickCustom(int interval = 100)
-        {
-            try
-            {
-                //MouseEvent(MouseEventLeftDown, 0, 0, 0, 0);
-                //MouseEvent(MouseEventLeftUp, 0, 0, 0, 0);
-                stoppingEvent.WaitOne(interval);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("MouseClickCustom\r\n" + e.Message);
-            }
-        }
-
+        // set mouse cursor point
         public void MouseSetPosCustom(int x, int y)
         {
             try
             {
                 SetCursorPos(x, y);
                 stoppingEvent.WaitOne(interval_);
-                MouseClickCustom(100);
             }
             catch (Exception e)
             {
